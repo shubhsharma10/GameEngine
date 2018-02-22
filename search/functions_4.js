ad44a3126448ba25367ddc00210049f2097a9d02
Babubhai <shubhsharma10@gmail.com>
2018-02-21 19:32:49 -0500

    Updated readme and docs

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['init',['init',['../class_ball.html#ac645db1f55e1fe17874b0aa0daa966b1',1,'Ball::init()'],['../class_brick.html#a88191494ceb4851bf8a627ec3c2d254a',1,'Brick::init()'],['../class_credit.html#a9e14b2c6995b083703438ce928190601',1,'Credit::init()'],['../class_instruction.html#acc1aabea623c6d6343a969d9a026acb5',1,'Instruction::init()'],['../class_l_texture.html#a9134649ea80eb374f4b1d988c731c0db',1,'LTexture::init()'],['../class_main_menu.html#a93d334473d065250d7cc955cf2a4bf97',1,'MainMenu::init()'],['../class_paddle.html#a1633cefd549bfc3ee1b985227abe4504',1,'Paddle::init()']]],
+  ['init',['init',['../class_ball.html#a5d07e21bfbdefc5ec6b66dfb36487de9',1,'Ball::init()'],['../class_brick.html#a88191494ceb4851bf8a627ec3c2d254a',1,'Brick::init()'],['../class_credit.html#a9e14b2c6995b083703438ce928190601',1,'Credit::init()'],['../class_instruction.html#acc1aabea623c6d6343a969d9a026acb5',1,'Instruction::init()'],['../class_l_texture.html#affc8f9ec7f06e633703b8a232b3dc71c',1,'LTexture::init()'],['../class_main_menu.html#a93d334473d065250d7cc955cf2a4bf97',1,'MainMenu::init()'],['../class_paddle.html#a8ded291609b0a6d7a3711b130362ee42',1,'Paddle::init()']]],
   ['instance',['instance',['../class_resource_manager.html#a91756683167c290b33b7525881122940',1,'ResourceManager']]],
   ['instruction',['Instruction',['../class_instruction.html#aebd15229c1651af49dcb203707e7a2d5',1,'Instruction']]],
   ['isballstationary',['isBallStationary',['../class_ball.html#aee6fe5a0dc76a429f23a87fdcf5a3f5c',1,'Ball']]],
